6740bf4f6dca8decd8fa7b1f21ef692185ef4620
Viktor Zhukov <v_zhukov1985@mail.ru>
2020-10-23 12:10:47 +0600

    Initial commit

---
+++ Tst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
